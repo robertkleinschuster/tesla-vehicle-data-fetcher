438c51d6fa165f442acb64c089aaadc944a818c8
robertkleinschuster <robert.kleinschuster@niceshops.com>
2020-07-19 14:38:44 +0200

    include jq

--- bin/check-dependencies.sh
+++ bin/check-dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if ! command -v jq &> /dev/null
-then
-    echo "Command jq could not be found. Install jq and run this script again."
-    exit 1
-fi
+#if ! command -v jq &> /dev/null
+#then
+#    echo "Command jq could not be found. Install jq and run this script again."
+#    exit 1
+#fi
