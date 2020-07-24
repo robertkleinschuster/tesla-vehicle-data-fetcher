5b9a640267bc53a8317b4825ac4b7720a5ae8ada
robertkleinschuster <robert.kleinschuster@niceshops.com>
2020-07-24 22:07:11 +0200

    only save if not empty

--- bin/update-vehicle-data.sh
+++ bin/update-vehicle-data.sh
@@ -4,6 +4,10 @@ bash "./bin/check-dependencies.sh"
 [ $? -eq 0 ] || exit 1
 id=$(bash "./bin/config/read.sh" -s api -k id)
 [ $? -eq 0 ] || exit 1
-bash "./bin/api/fetch-vehicle-data.sh" $id > "./data/vehicle_data.json"
+vehicle_data=$(bash "./bin/api/fetch-vehicle-data.sh" "$id");
+if [ -n "$vehicle_data" ]
+then
+echo "$vehicle_data" > "./data/vehicle_data.json"
+fi
 [ $? -eq 0 ] || exit 1
 exit 0
